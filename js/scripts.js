6e6ee415ab2cbb737aaf670c9f8dca116065c95f
Ashraf Hossan Shovo <ashrafshovo@gmail.com>
2022-02-19 02:28:53 +0600

    Contact section added

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,11 @@ $(document).ready(function(){
 	$(window).on('scroll load', function(){
 		$('#menu').removeClass('fa-times');
 		$('header').removeClass('toggle');
+		if($(window).scrollTop() > 0){
+			$('.top').show();
+		}else{
+			$('.top').hide();
+		}
 	});
 
 });
@@ -22,3 +27,19 @@ $(window).on("load",function(){
 });
 
 /*Loader ends*/
+
+/*Smooth scrolling starts*/
+
+$('a[href*="#"]').on('click', function(e){
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top,
+	},
+		500,
+		'linear'
+	);
+
+});
+
+/*Smooth scrooling ends*/
